33ff367b93a473c7d949f59aca6ff05196b992d3
GKO <mivalsten@gmail.com>
2022-11-30 16:40:53 +0100

    nb people in results

--- remove-peopleFromBallot.ps1
+++ remove-peopleFromBallot.ps1
@@ -65,24 +65,27 @@ foreach ($line in $fileContent) {
 
 $m = @()
 $k = @()
+$x = @()
 
 foreach ($votee in $constituencies) {
-    if ($votee[-2, -1] -join "" -eq ":M") {
-        $m += $votee
-    }
-    else {
-        $k += $votee
+    switch ($votee[-2, -1]) {
+        ":M" { $m += $votee; break }
+        ":K" { $k += $votee; break }
+        ":X" { $x += $votee; break }
+        Default {}
     }
 }
 $m -join "," | Out-File -Encoding UTF8 -FilePath $constituenciesPath
 $k -join "," | Out-File -Encoding UTF8 -FilePath $constituenciesPath -append
+$x -join "," | Out-File -Encoding UTF8 -FilePath $constituenciesPath -append
 
-
+Push-Location
 Set-Location $zeusPath
 New-Item -Path "$filepath\out\" -ItemType "directory" -ErrorAction SilentlyContinue | Out-Null
 
-python -m stv.stv -b $ballotsPath -c $constituenciesPath -q $quota -s $seats | out-file "$outpath.csv"
+C:\Users\mival\AppData\Local\Programs\Python\Python310\python.exe -m stv.stv -b $ballotsPath -c $constituenciesPath --separate-quota $quota -s $seats | out-file "$outpath.csv"
 Get-Content "$outpath.csv"
 
-& "$psscriptroot/format-zeusOutput.ps1" -file "$outpath.csv" -title "$title" | pandoc -s -o "$outpath.pdf"
+& "$psscriptroot/format-zeusOutput.ps1" -file "$outpath.csv" -title "$title" | Tee-Object "$outpath.md" | pandoc -s -o "$outpath.pdf"
+Pop-Location
 #>
--- zeus-fixEmail.ps1
+++ zeus-fixEmail.ps1
@@ -49,11 +49,13 @@ foreach ($e in $in) {
     $updateResult = ssh db3.razem "sudo su postgres -c `"~/update_email.sh $($e.poll) $newEmail $oldEmail`""
     if ($updateResult -match "UPDATE 1") {
         $x = (ssh db3.razem "sudo su postgres -c `"~/mail_uuid.sh $($e.poll) $newEmail`"")
+        $emailSubject = if (($in | ? { $_.name -eq $e.name }).count -eq 1) { $e.name } else { "$($e.name) - $($e.pollName)" }
+        Write-Output "Wysyłam maila wyborczego do $newEmail o wyborach $emailSubject"
         $r = Invoke-WebRequest -uri "$base/elections/$($e.election)/polls/$($e.poll)/voters/email/$x" -WebSession $session -method POST -Body @{
             "csrfmiddlewaretoken" = $csrfRegex.matches(($r.Content -split "`n" | select-string "csrfmiddlewaretoken")[0]).captures.groups[1].value
             "template"            = "vote"
             "voter_id"            = "$newEmail"
-            "email_subject"       = if (($in | ? { $_.name -eq $e.name }).count -eq 1) { $e.name } else { "$($e.name) - $($e.pollName)" }
+            "email_subject"       = $emailSubject
             "email_body"          = "W przypadku problemów z logowaniem <b>otwórz link do głosowania w trybie incognito</b>. W tym celu kliknij w link prawym przyciskiem myszy i wybierz `"Otwórz w trybie incognito`" lub podobnie brzmiącą opcję. W razie problemów, skontaktuj się ze swoim Zarządem Okręgu."
             "send_to"             = "all"
             "sms_body"            = ""
